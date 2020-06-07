121fa7f66a4a090a2b93d66176f263673748bdf2
PavelGem <pashentsev.1@gmail.com>
2020-06-07 18:10:15 +0300

    create bolvanka

---
+++ Final_lyceum_project/Assets/Scripts/CheckRight.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CheckRight : MonoBehaviour
+{
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (collision.gameObject.tag == "Ground")
+        {
+            GetComponentInParent<PlayerController>().isBodyTolcheckRight = true;
+        }
+    }
+    private void OnTriggerExit2D(Collider2D collision)
+    {
+        if (collision.gameObject.tag == "Ground")
+        {
+            GetComponentInParent<PlayerController>().isBodyTolcheckRight = false;
+        }
+    }
+}
---
+++ Final_lyceum_project/Assets/Scripts/PlayerController.cs
@@ -0,0 +1,65 @@
+﻿using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+    Rigidbody2D rigidbody;
+    public float speed;
+    public float jumpForce;
+    public int jumpCounter = 2;
+    public bool isBodyTolcheckRight = false;
+    public bool isBodyTolcheckLeft = false;
+    SpriteRenderer renderer;
+    Animator animator;
+
+    void Start()
+    {
+        rigidbody = GetComponent<Rigidbody2D>();
+        renderer = GetComponent<SpriteRenderer>();
+        animator = GetComponent<Animator>();
+    }
+
+
+    void Update()
+    {
+        if (transform.position.y < -15f)
+        {
+            transform.position = Vector3.zero;
+        }
+        if (Input.GetKey(KeyCode.D) && !isBodyTolcheckRight)
+        {
+            rigidbody.velocity = new Vector3(speed * Time.deltaTime, rigidbody.velocity.y);
+            renderer.flipX = false;
+        }
+        if (Input.GetKey(KeyCode.A) && !isBodyTolcheckLeft)
+        {
+            rigidbody.velocity = new Vector3(-speed * Time.deltaTime, rigidbody.velocity.y);
+            renderer.flipX = true;
+        }
+        if (Input.GetKeyDown(KeyCode.Space) && jumpCounter > 0)
+        {
+            rigidbody.AddForce(new Vector3(0, jumpForce * Time.deltaTime), ForceMode2D.Impulse);
+            jumpCounter--;
+        }
+        if (rigidbody.velocity.y>1f)
+        {
+            animator.Play("Jump");
+        }
+        else if (Mathf.Abs(rigidbody.velocity.x) > 2f)
+        {
+            animator.Play("Moveing");
+        }
+        else
+        {
+            animator.Play("Staying");
+        }
+
+    }
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        if (collision.gameObject.tag == "Ground")
+        {
+            jumpCounter = 2;
+        }
+    }
+
+}
